441f3f6b3142a3849982dc0378c10a2bbed5f69a
maccus-bo-wang <66904339+maccus-bo-wang@users.noreply.github.com>
2020-07-08 19:45:40 +0800

    Update install.sh

--- build/install.sh
+++ build/install.sh
@@ -1,3 +1,6 @@
 #Install dependencies, SFDX CLI in this case
 echo "Installing Dependencies... "
+sudo npm install lodash --save
+sudo npm cache clean
+echo "mmmmmmmm"
 sudo npm install -global sfdx-cli
